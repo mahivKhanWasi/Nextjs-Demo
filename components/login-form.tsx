24d282ae8d1c1129f4877e83691fb29e2b9eb264
mahivKhanWasi <wasi@project2morrow.com>
2025-01-15 11:47:52 +0600

    .

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,30 +1,19 @@
-"use client";
-
-import { useState } from "react";
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 export function LoginForm({
   className,
-  onSubmit,
   ...props
-}: React.ComponentPropsWithoutRef<"div"> & { onSubmit: (credentials: { email: string; password: string }) => void }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    onSubmit({ email, password });
-  };
+}: React.ComponentPropsWithoutRef<"div">) {
 
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
@@ -36,7 +25,7 @@ export function LoginForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form onSubmit={handleSubmit}>
+          <form >
             <div className="flex flex-col gap-6">
               <div className="grid gap-2">
                 <Label htmlFor="email">Email</Label>
@@ -44,8 +33,6 @@ export function LoginForm({
                   id="email"
                   type="email"
                   placeholder="m@example.com"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
                   required
                 />
               </div>
@@ -59,16 +46,13 @@ export function LoginForm({
                     Forgot your password?
                   </a>
                 </div>
-                <Input
-                  id="password"
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
+                <Input id="password" type="password" required />
               </div>
               <Button type="submit" className="w-full">
                 Login
+              </Button>
+              <Button variant="outline" className="w-full">
+                Login with Google
               </Button>
             </div>
             <div className="mt-4 text-center text-sm">
@@ -81,5 +65,5 @@ export function LoginForm({
         </CardContent>
       </Card>
     </div>
-  );
+  )
 }
